56350283cfa66c69a67b671db99e2af2c93e6e4e
fitrihandayani <fitrihandayani123@std.unissula.ac.id>
2020-02-20 11:00:15 +0700

    update 2

--- app/src/main/java/com/example/mynoteapp/MainActivity.kt
+++ app/src/main/java/com/example/mynoteapp/MainActivity.kt
@@ -9,6 +9,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        ///update
+        ///update 2222
     }
 }
